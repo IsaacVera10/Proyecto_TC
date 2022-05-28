fd1dab9e5aaee379d84ea6a1f30470b8acb48fa5
IsaacVera10 <isaac.vera@utec.edu.pe>
2022-05-28 09:29:53 -0500

    update

---
+++ cmake-build-debug/CMakeFiles/Proyecto_TC.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "D:/Proyecto_TC/Grafo.cpp" "CMakeFiles/Proyecto_TC.dir/Grafo.cpp.obj" "gcc" "CMakeFiles/Proyecto_TC.dir/Grafo.cpp.obj.d"
+  "D:/Proyecto_TC/main.cpp" "CMakeFiles/Proyecto_TC.dir/main.cpp.obj" "gcc" "CMakeFiles/Proyecto_TC.dir/main.cpp.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/Proyecto_TC.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Proyecto_TC.dir/Grafo.cpp.obj"
+  "CMakeFiles/Proyecto_TC.dir/Grafo.cpp.obj.d"
+  "CMakeFiles/Proyecto_TC.dir/main.cpp.obj"
+  "CMakeFiles/Proyecto_TC.dir/main.cpp.obj.d"
+  "Proyecto_TC.exe"
+  "Proyecto_TC.exe.manifest"
+  "Proyecto_TC.pdb"
+  "libProyecto_TC.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Proyecto_TC.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
