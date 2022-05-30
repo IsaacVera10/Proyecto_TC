2e65d0e20e3d9cd549ac7adf91b9432936fdfb91
IsaacVera10 <isaac.vera@utec.edu.pe>
2022-05-30 01:34:54 -0500

    pre_final

--- main.cpp
+++ main.cpp
@@ -21,6 +21,76 @@ bool  is_Correct(string T, string alfabeto){ // o(N^2)
     return true;
 }*/
 
+struct bloque{
+    int* array;
+    int size;
+    bloque(){
+        array = nullptr;
+        size = 0;
+    }
+    void resize(int tamanho){
+        int* nuevo = new int[tamanho];
+        if(array == nullptr){
+            array = nuevo;
+        }else{
+            for(int x = 0; x < size; ++x){
+                nuevo[x] = array[x];
+            }
+        }
+    }
+};
+
+
+//ejemplo: bloque* fila son bloques
+int igualdad(bloque* fila, int sizeFila, bloque& contenido){
+    bool var = true;
+
+    bloque temp;
+    for(int i = 0; i < sizeFila; ++i){
+        temp.array = fila[i].array;
+        temp.size = fila[i].size;
+
+        if(temp.size != contenido.size){
+            var=false;
+        }else{
+            for(int j=0;j<contenido.size;++j){//comparo cada caracter
+                if(contenido.array[j] != temp.array[j]){
+                    var = false;
+                }
+            }
+        }
+        if(var == true)
+            return i;
+
+        var=true;
+    }
+
+    return -1;
+}
+
+bloque calculate(bloque& arreglo, char caracter, grafo graph){
+    bloque result;
+
+    int cont = 0;
+    int indicator = 0;
+
+    for(int i=0;i<arreglo.size;i++){
+        int* temp= graph.get_alcance(arreglo.array[i], caracter, cont);
+
+        result.resize(result.size + cont);
+
+        for(int j=0;j<cont;j++){
+            result.array[indicator] = temp[j];
+            indicator++;
+            result.size++;
+        }
+        cont = 0;
+    }
+
+    return result;
+}
+
+
 
 
 grafo funcion_pregunta_1(string alfabeto, int num_Ts){
@@ -169,6 +239,14 @@ void Pregunta_2(){
     delete [] res;
 }
 
+
+bool is_Final(int* array, int size, int estado){
+    for(int x = 0; x < size; ++x){
+        if(estado == array[x]) return true;
+    }
+    return false;
+}
+
 void Pregunta_3(){
     string alfabeto;
     int num_Ts;
@@ -178,45 +256,107 @@ void Pregunta_3(){
     cout<<"Introduzca la cantidad de cadenas:";
     cin>> num_Ts;
 
-    grafo  a = funcion_pregunta_1(alfabeto, num_Ts);
+    grafo  AFN = funcion_pregunta_1(alfabeto, num_Ts);
 
-    int* matriz_dfn[a.get_size()][alfabeto.length()];
-    int size_dfn=0;
-    int count =0;
-
-    //Matriz de transiciones del AFN
-    for(int i=0;i<a.get_size();i++){
-        for(int j=0;j<alfabeto.length();j++){
-            string temp;
-            matriz_dfn[i][j] = a.get_alcance(i,alfabeto[j],size_dfn);
-            for(int k=0;k<size_dfn;k++){
-                cout<<*(matriz_dfn[i][j]+k)<<",";
-            }
-            size_dfn=0;
-            cout<<"   ";
-        }
-        cout<<endl;
+    int num_Ss;
+    cout << "Introduzca la cantidad de Ss: ";
+    cin>> num_Ss;
+    string* Ss = new string[num_Ss];
+    int contS = 0;
+    while(contS < num_Ss){
+        cin >> Ss[contS];
+        ++contS;
     }
 
-    //Matriz de transiciones AFD
-    //Estructura de la matriz del AFD
-    //filas:  array con estados
-    //Columna:
-    //Dentro de cada elemento de la matriz hay un array con los estados a llegar
+    //A lo mas se tendra |Vertices| * |alfabeto| filas en la matriz del AFD.
+    bloque* arrayBloque = new bloque[AFN.get_size()*alfabeto.length()];
+    int pos_array = 0; //Me indica la posicion dentro de la matrizFilas.
 
-    //A lo mas se tendra |Vertices| * |alfabeto| filas en la matriz del AFD. No me aseguro no necesitar un resize.
-    int* matrizAFD [a.get_size()*alfabeto.length()][alfabeto.length()];
-    int fila_current = 0;
-    /*
-        IDEAS SOBRE LA SOLUCION:
-        -
-     -
-    */
+    int* Efinales = new int[num_Ts];//Contiene los estados finales del AFD
+    int pos_EFinales = 0;
+
+    //Agrego el array con el estado inicial.
+    int* inicial = new int [1] {};
+    arrayBloque[pos_array].array = inicial;
+    arrayBloque[pos_array].size = 1;
+    ++pos_array;
+
+    int** matrizAFD = new int*[AFN.get_size()*alfabeto.length()];
+    for(int x = 0; x < AFN.get_size()*alfabeto.length(); ++x){
+        matrizAFD[x] = new int[alfabeto.length()];
+    }
+
+    int fCurrent = 0; //indica que fila estoy recorriendo.
+
+    //Por aqui esta el error
+    while(fCurrent < pos_array){
+        for (int j = 0; j < alfabeto.length(); j++) {
+            bloque llegada = calculate(arrayBloque[fCurrent], alfabeto[j], AFN);
+            int position = igualdad(arrayBloque, pos_array ,llegada);
+
+            if(position == -1){//Si no existe dentro de los vertices position = -1, entonces lo agrego a las filas.
+                position = pos_array;
+                arrayBloque[pos_array] = llegada;
+                ++pos_array;
+            }
+
+            matrizAFD[fCurrent][j] = position; //Registro la posicion dentro de la MatrizFilas.
+
+            //Compruebo para cada estado del array si alguno es final.
+            for(int x = 0; x < llegada.size; ++x){
+                if(is_Final(AFN, llegada.array[x])) {
+                    Efinales[pos_EFinales] = position;
+                    ++pos_EFinales;
+                }
+            }
+        }
+
+        ++fCurrent;
+    }
 
 
-    grafo result_dfn(10);
 
+    //Recorrido dentro del AFD
+    bool* res = new bool[num_Ss] {}; //Me indica si las cadenas fueron aceptadas o no, por defecto seran false;
 
+    for(int x = 0; x < num_Ss; ++x){//Se recorre cada cadena.
+        int state_current = 0; //Inicio en el estado 0
+
+        //Verifico si el estado 0 es un estado final
+        if(is_Final(Efinales, pos_EFinales, state_current)){
+            res[x] = true;
+            continue;
+        }
+
+        for(auto item: Ss[x]){//Se recorre cada elemento.
+            int posAlfabeto = alfabeto.find(item);
+            state_current = matrizAFD[state_current][posAlfabeto]; //Me indica a que estado transicionare.
+
+            //Verifico si es un estado final.
+            if(is_Final(Efinales, pos_EFinales, state_current)){
+                res[x] = true;
+                break;
+            }
+        }
+    }
+
+    //Impresion de si fue aceptado o no
+    for(int x = 0; x < num_Ss; ++x){
+        if(res[x])
+            cout << "SI" << endl;
+        else
+            cout << "NO" << endl;
+    }
+
+    //Limpiando la memoria.
+
+    for(int x = 0; x < AFN.get_size()*alfabeto.length(); ++x){
+        delete [] matrizAFD[x];
+    }
+    delete [] matrizAFD;
+    delete [] arrayBloque;
+    delete [] Efinales;
+    delete [] res;
 }
 
 
@@ -234,13 +374,16 @@ void ejemplo(){
     nuevo.setEfinal(1);
     nuevo.setEfinal(2);
 
-    nuevo.display();
-    for(int x = 0; x < 3; x++){
-        cout << x << " is Final " <<  is_Final(nuevo, x) << endl;
+    bloque a;
+    a.array = new int[1] {};//Array solo con el estado 0
+    a.size = 1;
+    bloque prueba = calculate(a, 'e', nuevo);
+
+    for(int x = 0; x < prueba.size; ++x){
+        cout << prueba.array[x] << " ";
     }
-    //Funcion is_Final funciona correctamente.
-
-
+    cout << endl;
+    //Salida esperada: 0 1 o tambien: 1 0
 }
 
 
@@ -270,6 +413,92 @@ void prueba_Alcance(){
     cout<<endl<<size;
 
 }
+
+void ejemplo1(){
+    string alfabeto;
+    int num_Ts;
+
+    cout<<"Introduzca el alfabeto:";
+    cin >> alfabeto;
+    cout<<"Introduzca la cantidad de cadenas:";
+    cin>> num_Ts;
+    grafo  a = funcion_pregunta_1(alfabeto, num_Ts);
+
+    bloque* MatrizFilas = new bloque[a.get_size()*alfabeto.length()];
+
+    int pos_Matriz = 0; //Me indica la posicion dentro de la matrizFilas.
+
+    int* Efinales = new int[num_Ts];//Contiene los estados finales del AFD
+    int pos_EFinales = 0;
+
+    //Agrego el array con el estado inicial.
+    int* inicial = new int [1] {};
+    MatrizFilas[pos_Matriz].array = inicial;
+    MatrizFilas[pos_Matriz].size = 1;
+    ++pos_Matriz;
+
+    for(int i=0;i<alfabeto.length();i++){
+        bloque llegada = calculate(MatrizFilas[0], alfabeto[i], a);
+        for(int j=0;j<llegada.size;j++){
+            cout<<llegada.array[j]<<" ";
+        }
+        cout<<endl;
+    }
+
+    bloque abc;
+    abc.array = inicial;
+    abc.size = 1;
+
+    if(igualdad(MatrizFilas,1 ,abc) == -1){
+        cout << "No esta en la matriz" << endl;
+    }else{
+        cout << "Esta en la matriz " << endl;
+    }
+
+
+    cout << endl;
+
+}
+
+
+void prueba_igualdad(){
+    bloque* ArrBloque = new bloque[2];
+    int* arr = new int[1];
+    arr[0] = 3;
+    ArrBloque[0].array = arr;
+    ArrBloque[0].size = 1;
+
+    int* arr2 = new int[2];
+    arr2[0] = 0;
+    arr2[1] = 1;
+    ArrBloque[1].array = arr2;
+    ArrBloque[1].size = 2;
+
+
+    bloque prueba;
+    prueba.array = arr2;
+    prueba.size = 2;
+
+    if(igualdad(ArrBloque,2 ,prueba) == -1){
+        cout << "No esta en la matriz" << endl;
+    }else{
+        cout << "Esta en la matriz " << endl;
+    }
+
+    bloque a;
+    int* ar = new int[3] {0,1,2};
+    a.array = ar;
+    a.size = 3;
+
+
+    if(igualdad(ArrBloque,2 ,a) == -1){
+        cout << "No esta en la matriz" << endl;
+    }else{
+        cout << "Esta en la matriz " << endl;
+    }
+
+}
+
 int main() {
     //Pregunta_1();
     //Pregunta_2();
@@ -278,6 +507,9 @@ int main() {
     //ejemplo();
     //prueba_Alcance();
     Pregunta_3();
+
+    //ejemplo1();
+    //prueba_igualdad();
 
     return 0;
 }
